48d6daa3c6a5e6b5d207abed9418fa300fcbcdc6
BurakSenol00 <buraksenol00@outlook.com>
2025-03-09 16:04:10 +0300

    Form created, user entries added, interface improved

--- BankAccount/Form1.Designer.cs
+++ BankAccount/Form1.Designer.cs
@@ -28,12 +28,152 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            textBox1 = new TextBox();
+            numericUpDown1 = new NumericUpDown();
+            numericUpDown2 = new NumericUpDown();
+            btnDeposit = new Button();
+            btnWithDraw = new Button();
+            btnCreateAccount = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            dataGridView1 = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(121, 29);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(150, 27);
+            textBox1.TabIndex = 0;
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(121, 79);
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(150, 27);
+            numericUpDown1.TabIndex = 1;
+            // 
+            // numericUpDown2
+            // 
+            numericUpDown2.Location = new Point(121, 183);
+            numericUpDown2.Name = "numericUpDown2";
+            numericUpDown2.Size = new Size(150, 27);
+            numericUpDown2.TabIndex = 1;
+            // 
+            // btnDeposit
+            // 
+            btnDeposit.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            btnDeposit.Location = new Point(27, 245);
+            btnDeposit.Name = "btnDeposit";
+            btnDeposit.Size = new Size(94, 36);
+            btnDeposit.TabIndex = 2;
+            btnDeposit.Text = "Deposit";
+            btnDeposit.UseVisualStyleBackColor = true;
+            // 
+            // btnWithDraw
+            // 
+            btnWithDraw.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            btnWithDraw.Location = new Point(177, 245);
+            btnWithDraw.Name = "btnWithDraw";
+            btnWithDraw.Size = new Size(94, 36);
+            btnWithDraw.TabIndex = 3;
+            btnWithDraw.Text = "Withdraw";
+            btnWithDraw.UseVisualStyleBackColor = true;
+            // 
+            // btnCreateAccount
+            // 
+            btnCreateAccount.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            btnCreateAccount.Location = new Point(122, 122);
+            btnCreateAccount.Name = "btnCreateAccount";
+            btnCreateAccount.Size = new Size(149, 36);
+            btnCreateAccount.TabIndex = 4;
+            btnCreateAccount.Text = "Create Account";
+            btnCreateAccount.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            label1.Location = new Point(39, 29);
+            label1.Name = "label1";
+            label1.Size = new Size(55, 20);
+            label1.TabIndex = 5;
+            label1.Text = "Owner";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            label2.Location = new Point(12, 86);
+            label2.Name = "label2";
+            label2.Size = new Size(102, 20);
+            label2.TabIndex = 6;
+            label2.Text = "Interent Rate";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            label3.Location = new Point(27, 185);
+            label3.Name = "label3";
+            label3.Size = new Size(67, 20);
+            label3.TabIndex = 7;
+            label3.Text = "Amount";
+            label3.TextAlign = ContentAlignment.TopRight;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.BackgroundColor = SystemColors.ButtonFace;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(289, 29);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(499, 252);
+            dataGridView1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.GradientInactiveCaption;
+            ClientSize = new Size(800, 292);
+            Controls.Add(dataGridView1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(btnCreateAccount);
+            Controls.Add(btnWithDraw);
+            Controls.Add(btnDeposit);
+            Controls.Add(numericUpDown2);
+            Controls.Add(numericUpDown1);
+            Controls.Add(textBox1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Bank Account";
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox textBox1;
+        private NumericUpDown numericUpDown1;
+        private NumericUpDown numericUpDown2;
+        private Button btnDeposit;
+        private Button btnWithDraw;
+        private Button btnCreateAccount;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private DataGridView dataGridView1;
     }
 }
